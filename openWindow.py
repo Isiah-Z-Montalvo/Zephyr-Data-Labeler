d6b6395b5f4cad43f822a981c35f60967c7caefb
Swift <swiftzephyr17@gmail.com>
2023-08-10 23:49:32 -0700

    Added menu bar

--- openWindow.py
+++ openWindow.py
@@ -4,7 +4,8 @@
 from tkinter import *
 from tkinter.ttk import *
 from tkinter.filedialog import askdirectory
-
+from PIL import Image, ImageTk
+"""
 # function to open a new window
 # on a button click
 def openNewWindow():
@@ -23,35 +24,40 @@ def openNewWindow():
 	# A Label widget to show in toplevel
 	Label(newWindow,
 		  text ="This is a new window").pack()
-
+"""
 def selectFolder():
 	path = askdirectory(title="Select Folder")
-
+"""
 def drawButton(master, attrs):
 	btn = Button(master,
+				 image = photoimage,
 				 text = attrs[0],
 				 command = attrs[1])
 	btn.pack(pady = 10)
-
+"""
 def drawWindow():
+	"""
 	btnAttr = {
 		"New_Window" : ["Open New Window", openNewWindow],
 		"Open_Folder" : ["Select a Folder", selectFolder]
 	}
-
+	"""
 	# creates a Tk() object
 	master = Tk()
+	master.title("Zephyr Data Labeler")
 
 	# sets the geometry of main
 	# root window
 	master.geometry("1000x1000")
 
-	label = Label(master,
-				  text ="Zephyr Data Labeler")
-	label.pack(pady = 10)
-	
-	drawButton(master, btnAttr["New_Window"])
-	drawButton(master, btnAttr["Open_Folder"])
+	menuBar = Menu(master)
+	fileMenu = Menu(menuBar, tearoff = 0)
+	menuBar.add_cascade(label ='File', menu = fileMenu)
+	fileMenu.add_command(label ='Open Folder', command = selectFolder)
 
+	#drawButton(master, btnAttr["New_Window"])
+	#drawButton(master, btnAttr["Open_Folder"])
+
+	master.config(menu = menuBar)
 	# mainloop, runs infinitely
 	mainloop()
